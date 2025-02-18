1b98e7dfcdf20d17437746b949d0b3cf23514e74
Jennifer Owen <jenniferowen753@gmail.com>
2025-02-18 06:34:20 +0000

    Add first notebooks

---
+++ python/utils/func_utils.py
@@ -0,0 +1,11 @@
+import time
+
+def time_func(func):
+    def wrapper(*args, **kwargs):
+        start_time = time.time()
+        result = func(*args, **kwargs)
+        end_time = time.time()
+        elapsed_time = end_time - start_time
+        print(f"Function '{func.__name__}' executed in {elapsed_time:.8f} seconds")
+        return result
+    return wrapper
